1f2c15f1be48a57fca9c4026f332b44f04a60bb4
ep250051019-cyber <ep250051019@iiti.ac.in>
2025-10-17 16:50:51 +0530

    Update Q1.cpp

--- Q1.cpp
+++ Q1.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+using namespace std;
 
+struct Item
+{
+    int id;
+    float cost;
+};
+
+int main() {
+    
+    Item arr[6];
+    for(int i = 0; i<6; i++)   //Randomly initializing first four elemts with data
+    {
+        arr[i].id = i+1;
+        arr[i].cost = (i+1)*15; 
+    }
+    cout << "IDs of items with cost more than 50 are-\n";
+    for(int i = 0; i < 6; i++)
+    {
+        if(arr[i].cost > 50)
+        {
+            cout << arr[i].id << endl;
+        }
+    }
+	// your code goes here
+
+}

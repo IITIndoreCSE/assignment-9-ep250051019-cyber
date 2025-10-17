0d04b875409f3bf1a451e1973177462b03661a06
ep250051019-cyber <ep250051019@iiti.ac.in>
2025-10-17 16:52:13 +0530

    Implement point collection and first quadrant check
    The program collects coordinates for 7 points and identifies those in the first quadrant.

--- Q3.cpp
+++ Q3.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+using namespace std;
 
+struct Point
+{
+    float x, y;
+};
+
+int main() {
+    
+    Point arr[7];
+    for(int i = 0; i < 7; i++)
+    {
+        float x, y;
+        cout << "Enter x coordinate for point" << i+1 << ":";
+        cin >> x;
+        cout << "Enter y coordinate for point" << i+1 << ":";
+        cin >> y;
+        arr[i] = {x, y};
+        
+    }
+    
+    cout << "Points that lie in first quadrant are-\n";
+    
+    for ( int i = 0; i < 7; i++)
+    {
+        if (arr[i].x > 0 && arr[i].y > 0)
+        {
+            cout << i+1 << endl;
+        }
+    }
+	// your code goes here
+
+}

5608aeb62fde7758127e2c778e04a95799cccb24
ep250051019-cyber <ep250051019@iiti.ac.in>
2025-10-17 16:51:23 +0530

    Add RGB color inversion functionality
    Initialized an array of Color structs with RGB values and inverted the RGB values for the first three elements.

--- Q2.cpp
+++ Q2.cpp
@@ -1,1 +1,44 @@
+#include <iostream>
+using namespace std;
 
+struct Color
+{
+    int red, green, blue;
+};
+
+int main() {
+    
+    Color arr[5];
+    cout << "Initial RGB values are..\n";
+    
+    
+    for(int i = 0; i < 5 ; i++) //Initializing with random data
+    {
+        arr[i].red = 255/5*(i+1);
+        arr[i].blue = 255/5*(i+1);
+        arr[i].green = 255/5*(i+1);
+    }
+    
+    for(int i = 0; i < 5; i++)
+    {
+        cout << "(" << arr[i].red << ", " << arr[i].green << ", " << arr[i].blue << ")\n";
+    }
+    
+    cout << "Inverting RGB values for first three elements..\n";
+    
+    for(int i = 0; i<3; i++)
+    {
+        arr[i].red = (255-arr[i].red);
+        arr[i].blue = (255-arr[i].blue);
+        arr[i].green = (255-arr[i].green);
+    }
+    
+    cout << "Printing new RGB values..\n";
+    
+    for(int i = 0; i < 5; i++)
+    {
+        cout << "(" << arr[i].red << ", " << arr[i].green << ", " << arr[i].blue << ")\n";
+    }
+	// your code goes here
+
+}

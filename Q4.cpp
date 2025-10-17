b7e5e016213f76bdd037cb867db5c048e2981b57
ep250051019-cyber <ep250051019@iiti.ac.in>
2025-10-17 16:52:39 +0530

    Implement monthly sales tracking and analysis
    Added functionality to record monthly sales and determine maximum and minimum sales.

--- Q4.cpp
+++ Q4.cpp
@@ -1,1 +1,51 @@
+#include <iostream>
+using namespace std;
 
+struct SalesRecord
+{
+    string month;
+    float amount;
+};
+
+int main() {
+    
+    SalesRecord year[12];
+    
+    for(int i = 0; i < 12; i++)
+    {
+        string name;
+        float sales;
+        cout << "Enter the month name:";
+        cin >> name;
+        cout << "Enter the amount:";
+        cin >> sales;
+        
+        year[i].month = name;
+        year[i].amount = sales;
+    }
+    int max_index=-1, min_index=-1;
+    float max = -1, min = 3000000000000;
+    
+    for(int i = 0; i < 12; i++)
+    {
+        if(year[i].amount > max)
+        {
+            max = year[i].amount;
+            max_index = i;
+        }
+    }
+    
+    for(int i = 0; i < 12; i++)
+    {
+        if(year[i].amount < min)
+        {
+            min = year[i].amount;
+            min_index = i;
+        }
+    }
+    
+    cout << "Maximum sales were reported in " << year[max_index].month << endl;
+    cout << "Minimum sales were reported in " << year[min_index].month << endl;
+	// your code goes here
+
+}
